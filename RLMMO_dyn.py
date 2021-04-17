641b1b2e5abad5ea2ffca7a786ef53d789a08b82
AAsmaou <aamadour@enssat.fr>
2021-04-17 17:35:49 +0200

    Edit RLMMO_dyn

--- RLMMO_dyn.py
+++ RLMMO_dyn.py
@@ -1,43 +1,18 @@
-# find minimum of coins to make a given change V
-import sys
+def rendu_monnaie_mem(P,X):
+  mem = [0]*(X+1)
+  return rendu_monnaie_mem_c(P,X,mem)
 
-# m is size of coins array (number of
-# different coins)
-def minCoins(coins, m, V):
-	
-	# table[i] will be storing the minimum
-	# number of coins required for i value.
-	# So table[V] will have result
-	table = [0 for i in range(V + 1)]
-
-	# Base case (If given value V is 0)
-	table[0] = 0
-
-	# Initialize all table values as Infinite
-	for i in range(1, V + 1):
-		table[i] = sys.maxsize
-
-	# Compute minimum coins required
-	# for all values from 1 to V
-	for i in range(1, V + 1):
-		
-		# Go through all coins smaller than i
-		for j in range(m):
-			if (coins[j] <= i):
-				sub_res = table[i - coins[j]]
-				if (sub_res != sys.maxsize and sub_res + 1 < table[i]):
-					table[i] = sub_res + 1
-                    #print (table[i])
-	
-	if table[V] == sys.maxsize:
-		return -1
-	
-	return table[V]
-
-# Driver Code
-if __name__ == "__main__":
-
-	coins = [10,9, 6, 5, 1]
-	m = len(coins)
-	V = 22
-	print("Minimum coins required is ",	minCoins(coins, m, V))
+def rendu_monnaie_mem_c(P,X,m):
+  if X==0:
+    return 0
+  elif m[X]>0:
+    return m[X]
+  else:
+    mini = 1000
+    for i in range(len(P)):
+      if P[i]<=X:
+        nb=1+rendu_monnaie_mem_c(P,X-P[i],m)
+        if nb<mini:
+          mini = nb
+          m[X] = mini
+  return mini
